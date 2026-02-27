4b5c85c4e860267d2f16716e38c8ee561be64680
Samuel Machado <samuel_emerenciano@estudante.sesisenai.org.br>
2026-02-26 22:13:17 -0300

    dadas

---
+++ ex002/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+
+
+</head>
+<body style="background-color: blue;">
+    <h1></h1>
+    <?php   
+        echo "Hoje é dia " . date("d/M/Y");
+    ?>
+    
+</body>
+</html>
